821a813117afe68e93634c4241b947111aacb70e
Marglodis Tovar <marglodistovar@gmail.com>
2025-10-14 21:47:27 -0300

    Add: AppNavigation

---
+++ app/src/main/java/com/mtovar/myactivitiesapp/navigation/AppNavigation.kt
@@ -0,0 +1,40 @@
+package com.mtovar.myactivitiesapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mtovar.myactivitiesapp.ui.ActivityViewModel
+import com.mtovar.myactivitiesapp.ui.screens.AddActivityScreen
+import com.mtovar.myactivitiesapp.ui.screens.HomeScreen
+
+sealed class AppScreen(val route: String) {
+    object Home : AppScreen("home")
+    object AddActivity : AppScreen("add_activity")
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    // Compartimos una unica instancia del ViewModel entre ls dos pantallas
+    val activityViewModel: ActivityViewModel = viewModel()
+
+    NavHost(navController = navController, startDestination = AppScreen.Home.route) {
+        composable(AppScreen.Home.route) {
+            HomeScreen(
+                viewModel = activityViewModel,
+                onAddActivity = {
+                    navController.navigate(AppScreen.AddActivity.route)
+                })
+        }
+        composable(AppScreen.AddActivity.route) {
+            AddActivityScreen(
+                viewModel = activityViewModel,
+                onActivityAdded = {
+                    navController.popBackStack() // Regresa a la pantalla anterior
+                })
+        }
+    }
+}
---
+++ app/src/main/java/com/mtovar/myactivitiesapp/ui/screens/HomeScreen.kt
@@ -0,0 +1,7 @@
+package com.mtovar.myactivitiesapp.ui.screens
+
+import androidx.compose.runtime.Composable
+import com.mtovar.myactivitiesapp.ui.ActivityViewModel
+
+@Composable
+fun HomeScreen(viewModel: ActivityViewModel, onAddActivityClick: () -> Unit){}
