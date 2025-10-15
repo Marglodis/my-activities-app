f360629bd3dbb0aac71eb1f7751175e8f3b8ee91
Marglodis Tovar <marglodistovar@gmail.com>
2025-10-14 22:05:30 -0300

    add: HomeScren

--- app/src/main/java/com/mtovar/myactivitiesapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mtovar/myactivitiesapp/ui/screens/HomeScreen.kt
@@ -1,7 +1,131 @@
 package com.mtovar.myactivitiesapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.mtovar.myactivitiesapp.data.Activity
+import com.mtovar.myactivitiesapp.ui.ActivityUiState
 import com.mtovar.myactivitiesapp.ui.ActivityViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(viewModel: ActivityViewModel, onAddActivityClick: () -> Unit){}
+fun HomeScreen(viewModel: ActivityViewModel, onAddActivityClick: () -> Unit) {
+    // Observamos el estado del viewModel de forma segura para el ciclo de vida
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Mis Actividades") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddActivityClick
+            ) {
+                Icon(
+                    Icons.Filled.Add, contentDescription = "Agregar Actividad"
+                )
+            }
+        }
+
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = androidx.compose.ui.Alignment.Center
+        ) {
+            // USamos when para reaccionar a los diferentes estado sde la UI
+            when (val state = uiState) {
+                is ActivityUiState.Loading -> CircularProgressIndicator()
+                is ActivityUiState.Empty -> EmptyState()
+                is ActivityUiState.Success -> ActivityList(activities = state.activities)
+
+            }
+        }
+    }
+
+}
+
+@Composable
+fun EmptyState() {
+    Text(
+        text = "No hay actividades registradas.\n¡Presiona '+' para agregar una!",
+        fontSize = 18.sp,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.secondary
+    )
+}
+
+@Composable
+fun ActivityList(activities: List<Activity>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(activities, key = { it.id }) { activity ->
+            ActivityItem(activity = activity)
+        }
+
+    }
+}
+
+@Composable
+fun ActivityItem(activity: Activity) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
+        Text(
+            text = activity.name,
+            style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "${activity.date} - ${activity.time}",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.secondary
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = activity.description, style = MaterialTheme.typography.bodyLarge)
+    }
+
+}
