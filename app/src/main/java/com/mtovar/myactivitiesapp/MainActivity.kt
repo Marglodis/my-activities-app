b11cc299c07fc4be068a407bfd08112474da71fe
Marglodis Tovar <marglodistovar@gmail.com>
2025-10-14 22:29:22 -0300

    add: MainActivity

--- app/src/main/java/com/mtovar/myactivitiesapp/MainActivity.kt
+++ app/src/main/java/com/mtovar/myactivitiesapp/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.mtovar.myactivitiesapp.navigation.AppNavigation
 import com.mtovar.myactivitiesapp.ui.theme.MyActivitiesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyActivitiesAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Aqui llamamanos al ssitema de navegación
+                    AppNavigation()
+                }
 
             }
         }
